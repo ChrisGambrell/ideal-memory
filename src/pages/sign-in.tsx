9ab9f22477fe955961e9073e26374ab56c0cebc0
Chris Gambrell <christopher@gambrell.info>
2023-04-01 11:17:48 -0400

    Add sign in functionality

--- src/pages/sign-in.tsx
+++ src/pages/sign-in.tsx
@@ -1,20 +1,29 @@
-import { Anchor, Button, Center, Container, PasswordInput, Stack, TextInput, Title } from "@mantine/core";
+import { Anchor, Button, Center, Container, PasswordInput, Stack, Text, TextInput, Title } from "@mantine/core";
 import { useForm, zodResolver } from "@mantine/form";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { z } from "zod";
+import { api } from "~/utils/api";
 
 export default function SignIn() {
+  const router = useRouter();
+
   const form = useForm({
-    initialValues: { email: "", password: "" },
+    initialValues: { root: "", email: "", password: "" },
     validate: zodResolver(
       z.object({
         email: z.string().email(),
-        password: z.string().min(6),
+        password: z.string().min(1),
       })
     ),
   });
 
-  const handleSubmit = (values: typeof form.values) => console.log(values);
+  const { mutate: signIn, isLoading: loadingSignIn } = api.auth.signIn.useMutation({
+    onSuccess: () => router.push("/"),
+    onError: (error) => form.setFieldError("root", error.message),
+  });
+
+  const handleSubmit = (values: typeof form.values) => signIn(values);
 
   return (
     <Container mt={16} size="xs">
@@ -25,10 +34,17 @@ export default function SignIn() {
           </Center>
           <TextInput label="Email address" placeholder="you@example.com" {...form.getInputProps("email")} />
           <PasswordInput label="Password" {...form.getInputProps("password")} />
+          {form.errors.root && (
+            <Text c="red" size="sm">
+              {form.errors.root}
+            </Text>
+          )}
           <Anchor component={Link} href="/sign-up">
             Don&apos;t have an account? Sign up
           </Anchor>
-          <Button type="submit">Sign in</Button>
+          <Button loading={loadingSignIn} type="submit">
+            Sign in
+          </Button>
         </Stack>
       </form>
     </Container>
--- src/server/api/routers/auth.ts
+++ src/server/api/routers/auth.ts
@@ -4,15 +4,13 @@ import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const authRouter = createTRPCRouter({
-  signIn: publicProcedure
-    .input(z.object({ id: z.string().cuid(), data: z.object({ completed: z.boolean().optional() }) }))
-    .mutation(async ({ ctx, input }) => {
-      const taskToUpdate = await ctx.prisma.task.findUnique({ where: { id: input.id } });
-      if (!taskToUpdate) throw new TRPCError({ code: "NOT_FOUND", message: "Task not found" });
+  signIn: publicProcedure.input(z.object({ email: z.string().email(), password: z.string().min(1) })).mutation(async ({ ctx, input }) => {
+    const user = await ctx.prisma.user.findUnique({ where: { email: input.email } });
+    if (!user) throw new TRPCError({ code: "NOT_FOUND", message: "User does not exist" });
 
-      const updatedTask = await ctx.prisma.task.update({ data: input.data, where: { id: input.id } });
-      return updatedTask;
-    }),
+    if (!(await bcrypt.compare(input.password, user.password))) throw new TRPCError({ code: "BAD_REQUEST", message: "Incorrect password" });
+    else return user;
+  }),
 
   signUp: publicProcedure
     .input(z.object({ name: z.string().min(2), email: z.string().email(), password: z.string().min(6) }))
