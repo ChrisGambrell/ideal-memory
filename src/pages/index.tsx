702d1088bc3ebec42b5a7842a26b8a4901f4a346
Chris Gambrell <christopher@gambrell.info>
2023-03-31 21:10:33 -0400

    Show task list

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,10 @@
 import Head from "next/head";
+import { api } from "~/utils/api";
 
 export default function Home() {
+  const { data: tasks = [], isLoading: loadingTasks } = api.tasks.getAll.useQuery();
+
+  if (loadingTasks) return <div>Loading tasks...</div>;
   return (
     <>
       <Head>
@@ -8,7 +12,13 @@ export default function Home() {
         <meta name="description" content="A task keeping application" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="">hello, world!</main>
+      <main>
+        <ul>
+          {tasks.map((task) => (
+            <li key={task.id}>{task.body}</li>
+          ))}
+        </ul>
+      </main>
     </>
   );
 }
