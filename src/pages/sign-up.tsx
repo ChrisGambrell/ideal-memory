275406be35729b8d8f62c6d06015f6e39ff0a594
Chris Gambrell <christopher@gambrell.info>
2023-04-01 09:55:10 -0400

    Add sign in page

---
+++ src/pages/sign-up.tsx
@@ -0,0 +1,20 @@
+import { Anchor, Button, Center, Container, PasswordInput, Stack, TextInput, Title } from "@mantine/core";
+import Link from "next/link";
+
+export default function SignUp() {
+  return (
+    <Container mt={16} size="xs">
+      <Stack spacing="sm">
+        <Center>
+          <Title order={2}>Sign up</Title>
+        </Center>
+        <TextInput label="Email address" placeholder="you@example.com" />
+        <PasswordInput label="Password" />
+        <Anchor component={Link} href="/sign-in">
+          Already have an account? Sign in!
+        </Anchor>
+        <Button>Sign up</Button>
+      </Stack>
+    </Container>
+  );
+}
