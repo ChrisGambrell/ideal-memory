55b131b26f2a6db578ed679c4660b7041b57ca40
Chris Gambrell <christopher@gambrell.info>
2023-04-01 10:35:02 -0400

    Add validators to form

--- src/pages/sign-in.tsx
+++ src/pages/sign-in.tsx
@@ -1,20 +1,36 @@
 import { Anchor, Button, Center, Container, PasswordInput, Stack, TextInput, Title } from "@mantine/core";
+import { useForm, zodResolver } from "@mantine/form";
 import Link from "next/link";
+import { z } from "zod";
 
 export default function SignIn() {
+  const form = useForm({
+    initialValues: { email: "", password: "" },
+    validate: zodResolver(
+      z.object({
+        email: z.string().email(),
+        password: z.string().min(6),
+      })
+    ),
+  });
+
+  const handleSubmit = (values: typeof form.values) => console.log(values);
+
   return (
     <Container mt={16} size="xs">
-      <Stack spacing="sm">
-        <Center>
-          <Title order={2}>Sign in</Title>
-        </Center>
-        <TextInput label="Email address" placeholder="you@example.com" />
-        <PasswordInput label="Password" />
-        <Anchor component={Link} href="/sign-up">
-          Don&apos;t have an account? Sign up
-        </Anchor>
-        <Button>Sign in</Button>
-      </Stack>
+      <form onSubmit={form.onSubmit(handleSubmit)}>
+        <Stack spacing="sm">
+          <Center>
+            <Title order={2}>Sign in</Title>
+          </Center>
+          <TextInput label="Email address" placeholder="you@example.com" {...form.getInputProps("email")} />
+          <PasswordInput label="Password" {...form.getInputProps("password")} />
+          <Anchor component={Link} href="/sign-up">
+            Don&apos;t have an account? Sign up
+          </Anchor>
+          <Button type="submit">Sign in</Button>
+        </Stack>
+      </form>
     </Container>
   );
 }
--- src/pages/sign-up.tsx
+++ src/pages/sign-up.tsx
@@ -1,21 +1,42 @@
 import { Anchor, Button, Center, Container, PasswordInput, Stack, TextInput, Title } from "@mantine/core";
+import { useForm, zodResolver } from "@mantine/form";
 import Link from "next/link";
+import { z } from "zod";
 
 export default function SignUp() {
+  const form = useForm({
+    initialValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+    validate: zodResolver(
+      z.object({
+        name: z.string().min(2),
+        email: z.string().email(),
+        password: z.string().min(6),
+      })
+    ),
+  });
+
+  const handleSubmit = (values: typeof form.values) => console.log(values);
+
   return (
     <Container mt={16} size="xs">
-      <Stack spacing="sm">
-        <Center>
-          <Title order={2}>Sign up</Title>
-        </Center>
-        <TextInput label="Full name" placeholder="Your name" />
-        <TextInput label="Email address" placeholder="you@example.com" />
-        <PasswordInput label="Password" />
-        <Anchor component={Link} href="/sign-in">
-          Already have an account? Sign in
-        </Anchor>
-        <Button>Sign up</Button>
-      </Stack>
+      <form onSubmit={form.onSubmit(handleSubmit)}>
+        <Stack spacing="sm">
+          <Center>
+            <Title order={2}>Sign up</Title>
+          </Center>
+          <TextInput label="Full name" placeholder="Your name" {...form.getInputProps("name")} />
+          <TextInput label="Email address" placeholder="you@example.com" {...form.getInputProps("email")} />
+          <PasswordInput label="Password" {...form.getInputProps("password")} />
+          <Anchor component={Link} href="/sign-in">
+            Already have an account? Sign in
+          </Anchor>
+          <Button type="submit">Sign up</Button>
+        </Stack>
+      </form>
     </Container>
   );
 }
