f1be02de48165131d5bf6f62cae8335562053635
Chris Gambrell <christopher@gambrell.info>
2023-03-31 22:04:03 -0400

    Add search field for tasks

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,20 @@
-import { ActionIcon, Checkbox, Container, Divider, Group, Stack } from "@mantine/core";
+import { ActionIcon, Checkbox, Container, Divider, Group, Stack, TextInput } from "@mantine/core";
 import { IconTrash } from "@tabler/icons-react";
 import Head from "next/head";
-import { Fragment, type ChangeEvent } from "react";
+import { Fragment, useState, type ChangeEvent } from "react";
 import { api } from "~/utils/api";
 
 export default function Home() {
+  const [taskFilter, setTaskFilter] = useState("");
+
   const ctx = api.useContext();
   const { data: tasks = [], isLoading: loadingTasks } = api.tasks.getAll.useQuery();
   const { mutate: updateTask } = api.tasks.updateById.useMutation({ onSuccess: () => ctx.tasks.invalidate() });
   const { mutate: deleteTask, isLoading: loadingDeleteTask } = api.tasks.deleteById.useMutation({
     onSuccess: () => ctx.tasks.invalidate(),
   });
+
+  const filteredTasks = tasks.filter(({ body }) => body.toLowerCase().includes(taskFilter.toLowerCase()));
 
   const toggleTask = (id: string) => (e: ChangeEvent<HTMLInputElement>) => updateTask({ id, data: { completed: e.target.checked } });
 
@@ -23,25 +27,28 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Container mt={16} size="xs">
-        <Stack spacing="xs">
-          {tasks.map((task, i) => (
-            <Fragment key={task.id}>
-              <Group position="apart">
-                <Checkbox key={task.id} label={task.body} checked={task.completed} onChange={toggleTask(task.id)} />
-                <ActionIcon
-                  color="red"
-                  loading={loadingDeleteTask}
-                  radius="xl"
-                  size="sm"
-                  variant="subtle"
-                  onClick={() => deleteTask({ id: task.id })}
-                >
-                  <IconTrash size="0.875rem" />
-                </ActionIcon>
-              </Group>
-              {i !== tasks.length - 1 && <Divider my={4} />}
-            </Fragment>
-          ))}
+        <Stack spacing="xl">
+          <TextInput placeholder="Search for tasks..." value={taskFilter} onChange={(e) => setTaskFilter(e.target.value)} />
+          <Stack spacing="xs">
+            {filteredTasks.map((task, i) => (
+              <Fragment key={task.id}>
+                <Group position="apart">
+                  <Checkbox key={task.id} label={task.body} checked={task.completed} onChange={toggleTask(task.id)} />
+                  <ActionIcon
+                    color="red"
+                    loading={loadingDeleteTask}
+                    radius="xl"
+                    size="sm"
+                    variant="subtle"
+                    onClick={() => deleteTask({ id: task.id })}
+                  >
+                    <IconTrash size="0.875rem" />
+                  </ActionIcon>
+                </Group>
+                {i !== tasks.length - 1 && <Divider my={4} />}
+              </Fragment>
+            ))}
+          </Stack>
         </Stack>
       </Container>
     </>
